cb12105471ed13b28194bee6bfe55c8f2ac9c494
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-01-03 16:35:05 +0000

    Add & Implement LivreRepository

---
+++ src/main/java/com/ipnet/tp/repositories/LivreRepository.java
@@ -0,0 +1,13 @@
+package com.ipnet.tp.repositories;
+
+
+import com.ipnet.tp.models.Livre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivreRepository  extends JpaRepository<Livre, Long> {
+
+
+    public Livre findByTitre(String titre);
+}

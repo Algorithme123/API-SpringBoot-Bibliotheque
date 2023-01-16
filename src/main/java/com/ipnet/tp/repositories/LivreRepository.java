4f60613f9503400bd8681127413de6134649ee98
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-01-16 05:03:07 +0000

    Add : methode find in LivreRepositoty

--- src/main/java/com/ipnet/tp/repositories/LivreRepository.java
+++ src/main/java/com/ipnet/tp/repositories/LivreRepository.java
@@ -3,5 +3,9 @@ package com.ipnet.tp.repositories;
 import com.ipnet.tp.models.Livre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LivreRepository extends JpaRepository<Livre, Long> {
+
+    List<Livre> findByNom(String nom);
 }
